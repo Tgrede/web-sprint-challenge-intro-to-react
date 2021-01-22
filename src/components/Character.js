02eac1a4ba10ab2686d3a90c53ce504d714a95a3
Trenten grede <Trenten.grede@gmail.com>
2021-01-22 12:56:50 -0600

    added some styling will have to retake

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,28 @@
 // Write your Character component here
 import React from 'react'
+import styled from 'styled-components'
 
 
 export default function Character(props){
      const {person} = props
 
      return (
-          <div>
+          <StyledCharacterTile>
                <p>
                  {person.name}   
                </p>
-          </div>     
+          </StyledCharacterTile>     
      )
 }
+
+const StyledCharacterTile = styled.div`
+     display:flex;
+     flex-direction:column;
+     font-size: 1.5rem;
+     box-align: center;
+     width:75%;
+     color: white;
+     background-color: grey;
+     opacity: .6
+`;
+     
